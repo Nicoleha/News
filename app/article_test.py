042eab1550a08b9de770bd5aad899dc7c20e1c87
Nicole Hategekimana <nicoleha127@gmail.com>
2019-02-15 16:34:12 +0200

    test for source successful

--- app/article_test.py
+++ app/article_test.py
@@ -1,3 +1,19 @@
 import unittest
 from models import article
 Article=article.Article
+
+class ArticleTest(unittest.TestCase):
+     '''
+    Test class to test the behaviour of the Article class
+    '''
+    def setUp(self):
+        '''
+        set up method that will run before every Test
+        '''
+        self.new_article=Article("Nicole","WSJ Wealth Adviser Briefing","Brief price gaps in equity trades cost investors about $2 billion a year","http://m.wsj.net/video/20190214/021419seibjoebiden/021419seibjoebiden_1280x720.jpg","https://www.wsj.com/video/waiting-for-biden-why-the-2020-field-is-incomplete-until-he-decides/688A19B5-161D-4FA6-8315-9C025298B3D5.html")
+
+    def test_article(self):
+        self.assertTrue(isinstance(self.new_source,Source))
+
+if __name__ == '__main__':
+    unittest.main()
